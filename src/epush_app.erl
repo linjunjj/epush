5f04b80987e901f1bd6d12c50f778a6a9067091c
terry-xiaoyu <506895667@qq.com>
2017-03-28 00:42:40 +0800

    remove AMQP from project

--- src/epush_app.erl
+++ src/epush_app.erl
@@ -71,61 +71,27 @@ start_worker(#{id:=Id, type:=apns, pool_size:=PoolSize, cert_file:=CertFile,
                                        feedback_fun = fun epush_apns:handle_apns_delete_subscription/1
                                       },
     apns:connect(eutil:to_atom(Id), ApnsConn),
-    gen_subscriber(apns, eutil:to_binary(Id), PoolSize, #{type=>apns, apns_name=>Id}),
     put_push_conf(Id, Conf);
 start_worker(#{id:=Id, type:=xiaomi, pkg_name:=PkgName, app_secret:=AppSecret,
                pool_size:=PoolSize}=Conf) ->
     State = #{type=>xiaomi, pkg_name=>PkgName, app_secret=>AppSecret},
-    {ok, _} = gen_subscriber(xiaomi, eutil:to_binary(Id), PoolSize, State),
     put_push_conf(Id, Conf);
 start_worker(#{id:=Id, type:=huawei, app_id:=AppId, app_secret:=AppSecret,
                pool_size:=PoolSize}=Conf) ->
     State = #{type=>huawei, app_id=>AppId, app_secret=>AppSecret},
-    {ok, _Pid} = gen_subscriber(huawei, eutil:to_binary(Id), PoolSize, State),
-    %Pid = gproc:where({n,l,{turtle,service,huawei_subscriber}}),
-    %ChildSpecs = supervisor:which_children(Pid),
-    %PoolSpec = lists:keyfind(pool, 1, ChildSpecs),
-    %{pool, PoolPid, supervisor, [turtle_subscriber_pool]} = PoolSpec,
-    %SubsriberSpecs = supervisor:which_children(PoolPid),
-    %Fun = fun({_, SubsriberPid, worker, [turtle_subscriber]}) ->
-    %              erlang:send_after(6000, SubsriberPid, refresh_access_token)
-    %      end,
-    %lists:foreach(Fun, SubsriberSpecs),
     put_push_conf(Id, Conf);
 start_worker(#{id:=Id, type:=fcm, api_key:=ApiKey, proxy:=Proxy,
                pool_size:=PoolSize}=Conf) ->
     State = #{type=>fcm, api_key=>eutil:to_binary(ApiKey), proxy=>Proxy},
-    {ok, _} = gen_subscriber(fcm, eutil:to_binary(Id), PoolSize, State),
     put_push_conf(Id, Conf);
 start_worker(#{id:=Id, type:=flyme, app_id:=AppId, app_secret:=AppSecret,
                pool_size:=PoolSize}=Conf) ->
     State = #{type=>flyme, app_id=>AppId, app_secret=>AppSecret},
-    {ok, _} = gen_subscriber(flyme, eutil:to_binary(Id), PoolSize, State),
     put_push_conf(Id, Conf);
 start_worker(#{id:=Id, type:=yunpian, apikey:=Apikey, pool_size:=PoolSize}=Conf) ->
     State = #{type=>yunpian, apikey=>Apikey},
-    {ok, _} = gen_subscriber(yunpian, eutil:to_binary(Id), PoolSize, State),
     put_push_conf(Id, Conf),
     ok.
-
-gen_subscriber(Type, Queue, PoolSize, State) ->
-    Mod = erlang:list_to_atom(lists:concat(["epush_", Type])),
-    Declarations = [#'queue.declare'{queue = Queue, durable = true}],
-    Config = #{
-      name => binary_to_atom(Queue, utf8),
-      connection => amqp_server,
-      function => fun Mod:loop/4,
-      handle_info => fun Mod:handle_info/2,
-      init_state => State,
-      declarations => Declarations,
-      subscriber_count => PoolSize,
-      prefetch_count => PoolSize,
-      consume_queue => Queue,
-      passive => false
-     },
-    ServiceSpec = turtle_service:child_spec(Config),
-    supervisor:start_child(turtle_sup, ServiceSpec).
-
 
 put_push_conf(Id, Conf) ->
     eutil:put_ets(epush_confs, {push_confs, eutil:to_binary(Id)}, Conf).
